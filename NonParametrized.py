Fixed Normalization function/ instantiated self.ndim and self.d properties into object's constructor/ wrote function to plot walkers after mcmc is ran

NonParametrized.py
@@ -3,14 +3,20 @@ import matplotlib.pyplot as plt
 import emcee
 import scipy
 import scipy.integrate
+import seaborn as sns
+import pandas as pn
 
 
 class NonParametrized(object):
 
     # TODO: will do later once I have a better idea of what properties an object of this class needs
-    def __init__(self, ndim):
+    def __init__(self, ndim, d_bins):
         self.sampler = 0
+        # the user who initialized the NonParametrized object should have an idea
+        # of the dimension of the parameter space
         self.ndim = ndim
+        # how many bins of particle diameter sizes a user would like to infer
+        self.d = np.zeros(d_bins)
 
     @staticmethod
     def g2(f, d, y, stuff, time):
@@ -82,14 +88,14 @@ class NonParametrized(object):
             return -np.inf
         # if a value of f was negative, then the prior is zero, and ln(0) is infinity
 #######################################################################################
-
+    # TODO: Needs to rewrite likelihood to give the correct model
     # needs to redefine likelihood function
     # before form? after integration form?
 
-    def log_likelihood(self, f, d, y, stuff, time):
+    def log_likelihood(self, f, y, stuff, time):
         m, c, delta_d, eta, n, theta, k_b, temp, lambda_0, beta = stuff
 
-        g2_result = self.g2(f, d, stuff, time)
+        g2_result = self.g2(f, self.d, stuff, time)
 
         # usually, this parameter is given as part of the instrumentation
         sig_y = 1e-2  # infer noise variance
@@ -114,14 +120,13 @@ class NonParametrized(object):
     ###################################################################################
     # dont normalize g1, normalize the denominator thing! no tau dependence
     # TODO: fix
+    # DONE - Thy 6/17/19 3:49pm
     @staticmethod
-    def normalize(f, d, stuff, time):
+    def normalize(f, mie_fraction):
         # this function normalizes the integral of g(1) before the inference stage
         # returns the normalization constant that sticks to the front of the g(1) integral
-        m, c, delta_d, eta, n, theta, k_b, temp, lambda_0, beta = stuff
 
-        gamma = (16 * np.pi * (n ** 2) * ((np.sin(theta / 2)) ** 2) * k_b * temp) / (3 * eta * lambda_0 ** 2)
-        g1_integrand = f * np.e ** (-(gamma / d) * time)
+        g1_integrand = f*mie_fraction
         integral = scipy.integrate.trapz(g1_integrand)
 
         normalizationconstant = 1 / integral
@@ -130,8 +135,8 @@ class NonParametrized(object):
 
     ####################################################################################
 
-    def log_posterior(self, f, d, y, stuff, time):
-        return self.log_prior(f) + self.log_likelihood(f, d, y, stuff, time)
+    def log_posterior(self, f, y, stuff, time):
+        return self.log_prior(f) + self.log_likelihood(f, self.d, y, stuff, time)
 
     ################################################################################
 
@@ -139,17 +144,39 @@ class NonParametrized(object):
         prelim_pos = np.zeros(self.ndim)
         start_pos = [prelim_pos + 1e-4*np.random.randn(self.ndim)]
 
-        self.sampler = emcee.EnsembleSampler(nwalkers, self.ndim, self.log_posterior, args=(g2_data, d, stuff, time))
+        self.sampler = emcee.EnsembleSampler(nwalkers, self.ndim, self.log_posterior, args=(g2_data, self.d, stuff, time))
         self.sampler.run_mcmc(start_pos, nsteps)
 
     ####################################
 
     # def plot_sampler(self):
     # still not sure how to automate this plotting process
-    # TODO
+    # TODO: Implement stub function correctly? Figure out how to automate the plotting process of the walkers
     # stub function?
     # find the burn-in average
     # OK SO we can write a stub function that plots a thing that is called everytime something needs to be plotted
     # can be computationally expensive
 
-    
+    ## Stub function for plotting
+    def walker_plot(self):
+        plt.style.use('seaborn-deep')
+        plt.rcParams['mathtext.fontset'] = 'stix'
+        plt.rcParams['font.family'] = 'STIXGeneral'
+        params = {'legend.fontsize': 'x-large',
+                  'figure.figsize': (15, 5),
+                  'axes.labelsize': 'x-large',
+                  'axes.titlesize': 'x-large',
+                  'xtick.labelsize': 'x-large',
+                  'ytick.labelsize': 'x-large'}
+        axes = np.array([])
+        fig, axes = plt.subplots(self.ndim)
+        for i in range(self.ndim):
+            axes[i].set(ylabel='f of ' + self.d[i])
+
+        for i in range(self.ndim):  # type: int
+            for j in range(10):
+                sns.tsplot(self.sampler.chain[j, :, i], ax=axes[i])
+
+    #########################################################
+
+    # a function to find the burn in average?