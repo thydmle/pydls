883f7e96a71837bb7f1d625908ae968d948eea00
thydmle <tle3@ithaca.edu>
2019-07-01 10:54:48 -0400

    Changed g2 implementation to make sure that output distribution is normalized

--- dlsfunctions.py
+++ dlsfunctions.py
@@ -66,6 +66,7 @@ def g2(theta, d, m, gamma, time):
     delta_d = d[1] - d[0]
     for i in range(size):
         expo = np.exp(-(gamma*time[i])/d)
+        f = f*normalize(f, 1, delta_d)
         sum_squared = (np.sum(f*expo*delta_d))**2
         g2[i] = beta*sum_squared
     return g2
