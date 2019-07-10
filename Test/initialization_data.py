466af18487d06adc549bf3734dedb9a8132bbf8d
thydmle <tle3@ithaca.edu>
2019-07-10 13:05:18 -0400

    New tests

--- Test/initialization_data.py
+++ Test/initialization_data.py
@@ -16,8 +16,12 @@ r = 2.2439608235145855e-09
 sigma = 2e-10
 
 
-def get_time():
+def get_lin_time():
     return np.linspace(1e-4, 1e-1, 200)*0.001
+
+
+def get_log_time():
+    return np.logspace(-4, -1, num=200, base=10)*0.001
 
 
 def get_distance():
