aaea9361605e2b193d958b863c876eceae5b6726
thydmle <tle3@ithaca.edu>
2019-08-28 20:48:09 -0400

    Writeup complete

--- Test/dlsfunctions.py
+++ Test/dlsfunctions.py
@@ -10,6 +10,15 @@ import scipy.integrate
 
 # function returns the normalization constant
 # for a distribution
+# VARIABLES:
+#       f : distribution that needs normalization
+#       mie_fraction : Mie scattering fraction associated with the dls conditions for a specific trial
+#       delta_d : the step size of the particle-size bins
+#       g1_integrand : the product between the distribution f and the Mie fraction
+#       intergal : the result of the integration using scipy.integrate.trapz
+#       normconst : the normalization constant that is eventually returned
+# RETURNS:
+#       normconst : normalization constant to multiply to the input f distribution to normalize the distribution
 def normalize(f, mie_fraction, delta_d):
     g1_integrad = f*mie_fraction
     integral = scipy.integrate.trapz(g1_integrad, dx=delta_d)
@@ -18,12 +27,23 @@ def normalize(f, mie_fraction, delta_d):
 
 
 # function checks if a distribution is normalized
-# RETURNS: 1 if normalized; something else otherwise
+# VARIABLES:
+#       none
+# RETURNS:
+#       1 if normalized; something else otherwise
 def check_distribution_norm(f, delta_d):
     return scipy.integrate.trapz(f, dx=delta_d)
 
 
-# function generates a normalized distribution
+# function generates a normalized distribution around a given range of particle size bins
+# INPUT PARAMETERS:
+#       d : range of particle sizes
+#       mean :
+# VARIABLES:
+#       f : variable to hold the distribution generated. Unimodal distribution only
+#       normconst : normalization constant for the distribution
+# RETURNS:
+#       f : normalized, unimodal distribution of particle sizes in range of d
 def generate_distribution(d, mean, sigma, mie_fract):
     f = (1/(sigma*np.sqrt(2*np.pi)))*np.exp(-0.5*((d-mean)/sigma)**2)
     normconst = normalize(f, mie_fract, d[1]-d[0])
@@ -31,6 +51,9 @@ def generate_distribution(d, mean, sigma, mie_fract):
     return f
 
 
+# function generates normalized bimodal distribution with a given range of particle size bins
+# VARIABLES:
+#       f1
 def generate_bimodal_distribution(d, mean1, sigma1, mean2, sigma2, mie_fract):
     f1 = 1 / (sigma1 * np.sqrt(2 * np.pi ** 2)) * np.exp(-(d - mean1) ** 2 / (2 * sigma1 ** 2))
     f1 = f1 * normalize(f1, 1, d[1] - d[0])
@@ -68,21 +91,8 @@ def single_exponential_fit(t, C, const, B):
     return const*np.exp(-C*t) + B
 
 
-# TODO: this function also depends on the Mie fraction in the exponential term
-# that will need to be included once the Mie fraction's calculations are computed
-#def g2(f, d, beta, gamma, time):
-
-#    size = len(time)
-#    g2 = np.zeros(size)
-#    delta_d = d[1] - d[0]
-#
-#    for i in range(size):
-#        expo = np.exp(-(gamma*time[i])/d)
-#        sum_squared = (np.sum(f*expo*delta_d))**2
-#        g2[i] = beta*sum_squared
-#    return g2
-
-def g2(theta, d, m, gamma, time):
+def g2(theta, d, gamma, time):
+    m = len(d)
     beta = theta[m]
     f = theta[0:m]
     size = len(time)
@@ -154,7 +164,7 @@ def log_posterior(theta, d, y, m, gamma, time):
     # theta will be an array of size (m+1, )
     # log_prior and log_likelihood will need to slice theta correctly
 
-    return log_prior(theta, m) + log_prior_beta(theta, m) + log_likelihood(theta, d, y, m, gamma, time)
+    return log_prior(theta, m) + log_prior_beta(theta, m) + log_likelihood(theta, d, y, gamma, time)
 
 
 def create_start_pos(theta, ndim, nwalkers):
