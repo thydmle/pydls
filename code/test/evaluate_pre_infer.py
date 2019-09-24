1f322cc1bfd1cde9850291b1621eeaa4d92f67ef
thydmle <tle3@ithaca.edu>
2019-09-24 14:24:29 -0400

    non-important text changes

--- code/test/evaluate_pre_infer.py
+++ code/test/evaluate_pre_infer.py
@@ -1,17 +1,19 @@
+
+import sys
+sys.path.insert(0, '/Desktop/Personal/Research/SU2019/pydls/code/infer')
 import numpy as np
 import scipy
 import scipy.integrate
-from infer import pre_infer
+from infer.pre_infer import normalize, generate_distribution
 
 
 d = []
 for i in range(21):
-    d[i] = np.linspace(i * 10**-10, i * 10**-8)
+    d.append(np.linspace(i * 10**-10, i * 10**-8))
 
 mean = []
 for i in range(21):
-    mean[i] = np.average(d[i])
-
+    mean.append(np.average(d[i]))
 
 sigma = 2e-10
 
@@ -29,7 +31,7 @@ def test_normalize():
         test_f[i] = generate_nonnormal_distr(d[i], mean[i], sigma)
     for i in range(len(test_f)):
         test_constants[i] = 1 / (scipy.integrate.trapz(test_f[i], d[i][1] - d[i][0]))
-        norm_constants[i] = pre_infer.normalize(test_f[i], 1, d[i][1] - d[i][0])
+        norm_constants[i] = normalize(test_f[i], 1, d[i][1] - d[i][0])
         assert test_constants[i] == norm_constants[i]
 
 
@@ -41,7 +43,11 @@ def test_generate_distr():
         norm = 1 / (scipy.integrate.trapz(test_f[i], d[i][1] - d[i][0]))
         test_f[i] = test_f[i] * norm
 
-        control_f[i] = pre_infer.generate_distribution(d[i], mean[i], sigma, 1)
+        control_f[i] = generate_distribution(d[i], mean[i], sigma, 1)
 
         assert test_f[i] == control_f[i]
+
+
+def main():
+    test_normalize()
 
