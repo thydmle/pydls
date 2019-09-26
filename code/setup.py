4b1ef6b4ccf14eceb8f1c0aeb7d88bc645cc26be
thydmle <tle3@ithaca.edu>
2019-09-26 19:03:57 -0400

    All tests passed in test_pre_infer.py

---
+++ code/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="infer", packages=find_packages())
--- code/tests/test_pre_infer.py
+++ code/tests/test_pre_infer.py
@@ -1,53 +1,54 @@
-import sys
-sys.path.insert(0, "/Desktop/Personal/Research/SU2019/pydls/code")
-from infer.pre_infer import normalize, generate_distribution
+# import sys
+# sys.path.append("/Desktop/Personal/Research/dls/pydls/code/infer")
+import infer.pre_infer as pre_infer
 import numpy as np
 import scipy
 import scipy.integrate
 
 
-
 d = []
-for i in range(21):
-    d.append(np.linspace(i * 10**-10, i * 10**-8))
+for i in range(10):
+    d.append(np.linspace(i, (i + 9), 10))
 
 mean = []
-for i in range(21):
-    mean.append(np.average(d[i]))
+for a in range(10):
+    mean.append(d[a][8] - d[a][0])
 
-sigma = 2e-10
+sigma = []
+for b in range(10):
+    sigma.append(1e-1)
+
+counter_distributions = [] #? not sure how to name distributions calculated here
+for c in range(10):
+    f1 = (1/(sigma[c] * np.sqrt(2*np.pi)))*np.exp(-0.5*((d[c] - mean[c])/sigma[c])**2)
+    counter_distributions.append(f1)
+
+counter_constants = []
+for g in range(10):
+    counter_constants.append(1/(scipy.integrate.trapz(counter_distributions[g], dx=d[0][1] - d[0][0])))
 
 
-def generate_nonnormal_distr(d_in, mean_in, sigma_in):
-    test_f = (1/(sigma_in * np.sqrt(2 * np.pi))) * np.exp(-0.5*((d_in - mean_in) / sigma_in**2))
-    return test_f
+test_distributions = []
+for e in range(10):
+    test_distributions.append(pre_infer.generate_distribution(d[e], mean[e], sigma[e], 1))
+
+test_constants = []
+for f in range(10):
+    test_constants.append(pre_infer.normalize(counter_distributions[f], 1, d[f][1] - d[f][0]))
+
+for t in range(10):
+    counter_distributions[t] = counter_constants[t] * counter_distributions[t]
 
 
 def test_normalize():
-    test_f = []
-    test_constants = []
-    norm_constants = []
-    for i in range(len(d)):
-        test_f.append(generate_nonnormal_distr(d[i], mean[i], sigma))
-    for i in range(len(test_f)):
-        test_constants.append(1 / (scipy.integrate.trapz(test_f[i], d[i][1] - d[i][0])))
-        norm_constants.append(normalize(test_f[i], 1, d[i][1] - d[i][0]))
-        assert test_constants[i] == norm_constants[i]
+    for h in range(10):
+        assert counter_constants[h] == test_constants[h]
 
 
-def test_generate_distr():
-    test_f = []
-    control_f = []
-    for i in range(len(d)):
-        test_f[i] = (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((d[i] - mean[i]) / sigma)**2)
-        norm = 1 / (scipy.integrate.trapz(test_f[i], d[i][1] - d[i][0]))
-        test_f[i] = test_f[i] * norm
-
-        control_f[i] = generate_distribution(d[i], mean[i], sigma, 1)
-
-        assert test_f[i] == control_f[i]
-
-
-def main():
-    test_normalize()
+def test_distr_generator():
+    for j in range(10):
+        current_test = test_distributions[j]
+        current_counter = counter_distributions[j]
+        for k in range(10):
+            assert current_test[k] == current_counter[k]
 
